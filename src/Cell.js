857edd3373076ee3144a1edda60dca690d04f10e
DDarrow123 <ddarrowphl@gmail.com>
2018-11-20 16:43:41 -0500

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+
+  handleClick = () => {
+    console.log('Clicked!')
+    // const newColor = this.props.getSingleColor()
+    this.setState({
+      color: this.props.newColor
+    })
+  }
+
   render() {
+    console.log(this.props.selectedColor);
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell"
+        onClick={this.handleClick}
+        style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
-  
+
 }
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      const newColorChoice = () => {
+        console.log("I'm here in the color selector", str);
+        this.props.setSelectedColor(str)
+      }
+      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={newColorChoice}/>
     })
   )
-  
+
+
   render() {
+    console.log(this.props.setSelectedColor);
     return (
       <div id="colorSelector">
         {this.makeColorSwatches()}
       </div>
     )
   }
-  
+  debugger
+
 }
