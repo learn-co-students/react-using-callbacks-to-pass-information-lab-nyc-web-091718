9d82c3b492f5ebc8bb5e2b57533d9eb2c4ef6b14
Roger Vera <rovera@vassar.edu>
2018-11-20 14:10:21 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,21 +7,32 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      color: 'FFF'
+    }
   }
 
+  // Color selector needs to use this
+  changeColor = (hexColor) => {
+    this.setState({
+      color: hexColor
+    })
+  }
+
+  // Make each pixel (Cell)
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} currentColor={this.state.color} />)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
-
+// Why can't you just export method to child class?
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector changecolor={this.changeColor} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
